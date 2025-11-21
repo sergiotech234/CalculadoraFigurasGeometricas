614eba20fb42cb5ee6df6674daef939bfa9ebcb1
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-21 19:45:42 +0100

    Update Figura.java
    inserccion de comentarios

--- src/Figura.java
+++ src/Figura.java
@@ -3,6 +3,7 @@ public abstract class Figura implements Dibujable {
     public abstract double perimetro();
     public abstract void dibujar();
 
+     //creamos el metodo para que se pueda ver los datos en consola
     public void mostrar() {
         System.out.println("Figura area: " + area());
         System.out.println("Figura perimetro: " + perimetro());
