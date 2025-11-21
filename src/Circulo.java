2289eea1388ca805715bd5b7c9984afcd1c404ff
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-21 19:43:42 +0100

    Update Circulo.java
    introduccion de comentarios

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,17 +1,19 @@
+//Unimos la clase abstracta con la clase circulo
 public class Circulo extends Figura{
     private double radio;
-
+    
+//Creamos los getters y setter del radio del circulo
     public Circulo(double radio) {
         this.radio = radio;
     }
-
     public double getRadio() {
         return radio;
     }
-
     public void setRadio(double radio) {
         this.radio = radio;
     }
+    
+    //creamos los metodos para hacer el area y el perimetro del circulo
     @Override
     public double area() {
         return radio*radio;
@@ -20,11 +22,14 @@ public class Circulo extends Figura{
     public double perimetro() {
         return 2*Math.PI*radio;
     }
+    
+    //creamos el metodo para que se pueda ver los datos en consola
     @Override
     public void mostrar() {
         super.mostrar();
     }
 
+    //creamos la interzar dibujable para que lo muestre en la terminal
     @Override
     public void dibujar() {
         System.out.println("======");
